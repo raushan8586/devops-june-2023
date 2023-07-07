ae25ffc85c95a46d923877ab8c6d3fc289c1d735
raushan8586 <72825046+raushan8586@users.noreply.github.com>
2023-07-07 09:11:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,16 +9,44 @@ terraform {
 
 provider "aws" {
   region     = "ap-south-1"
-  access_key = "*****"
-  secret_key = "*****"
+  access_key = "AKIA4LM34PDX5IBQK5MQ"
+  secret_key = "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"
 }
 
-
+#create VPC
 resource "aws_vpc" "intellipaat-vpc" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
 
   tags = {
     Name = "intellipaat-vpc"
+  }
+}
+
+#create subnets
+resource "aws_subnet" "public-subnet" {
+  vpc_id     = aws_vpc.intellipaat-vpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "public-subnet"
+  }
+}
+
+resource "aws_subnet" "private-subnet" {
+  vpc_id     = aws_vpc.intellipaat-vpc.id
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "private-subnet"
+  }
+}
+
+#create internet gateway
+resource "aws_internet_gateway" "intellipaat-IGW" {
+  vpc_id = aws_vpc.intellipaat-vpc.id
+
+  tags = {
+    Name = "intellipaat-IGW"
   }
 }
